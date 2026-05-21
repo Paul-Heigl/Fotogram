f7bdd2b36eebdc35d1c5518f9951595603428cb0
Paul Heigl <paul@heigl.wien>
2026-05-21 11:06:14 +0200

    Commit mit Anpassungen

--- fotogram/script.js
+++ fotogram/script.js
@@ -1,65 +1,133 @@
-let dialog = document.getElementById('myDialog');
-let closeBtn = document.getElementById('closeBtn');
-let myImgs = ["1.png","2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png"];
-let myTitles = ["Berglandschaft","Stadtbild","Drittes Wolkenhimmel","Spatz","Weltall","Alpen","Vogel","Epischer Stein","Meise","Leoparden Baby","Berge","Baum"];
+let dialog = document.getElementById("myDialog");
+let closeBtn = document.getElementById("closeBtn");
+
+let images = [
+  {
+    img: "1.png",
+    title: "Berglandschaft",
+  },
+  {
+    img: "2.png",
+    title: "Stadtbild",
+  },
+  {
+    img: "3.png",
+    title: "Drittes Wolkenhimmel",
+  },
+  {
+    img: "4.png",
+    title: "Spatz",
+  },
+  {
+    img: "5.png",
+    title: "Weltall",
+  },
+  {
+    img: "6.png",
+    title: "Alpen",
+  },
+  {
+    img: "7.png",
+    title: "Vogel",
+  },
+  {
+    img: "8.png",
+    title: "Epischer Stein",
+  },
+  {
+    img: "9.png",
+    title: "Meise",
+  },
+  {
+    img: "10.png",
+    title: "Leoparden Baby",
+  },
+  {
+    img: "11.png",
+    title: "Berge",
+  },
+  {
+    img: "12.png",
+    title: "Baum",
+  },
+];
+
 let currentIndex = 0;
 
-function openDialog(imgName) {
-    let dialog = document.getElementById('myDialog');
-    let showPicture = document.getElementById('show_picture');
-    let showText = document.getElementById('show_text');
+function renderImages() {
+  let fotoSection = document.getElementById("foto_section");
 
-    showText.textContent = myTitles[currentIndex];
+  fotoSection.innerHTML = "";
 
-    showPicture.src = "./img/" + imgName;
-
-    showText.src = imgName;
-
-    currentIndex = myImgs.indexOf(imgName);
-
-    dialog.showModal();
-    document.body.classList.add('dialog-open');
-    updateCounter();
-    updateTitle();
+  for (let i = 0; i < images.length; i++) {
+    fotoSection.innerHTML += `
+            <button onclick="openDialog(${i})">
+                <img src="./img/${images[i].img}" alt="${images[i].title}">
+            </button>
+        `;
+  }
 }
 
-closeBtn.addEventListener('click', function () {
+function openDialog(index) {
+  let showPicture = document.getElementById("show_picture");
+  let showText = document.getElementById("show_text");
+
+  currentIndex = index;
+
+  showPicture.src = "./img/" + images[currentIndex].img;
+  showText.textContent = images[currentIndex].title;
+
+  dialog.showModal();
+
+  document.body.classList.add("dialog-open");
+
+  updateCounter();
+}
+
+closeBtn.addEventListener("click", function () {
+  dialog.close();
+});
+
+dialog.addEventListener("close", function () {
+  document.body.classList.remove("dialog-open");
+});
+
+dialog.addEventListener("click", function (event) {
+  if (event.target === dialog) {
     dialog.close();
+  }
 });
 
-dialog.addEventListener('close', function () {
-    document.body.classList.remove('dialog-open');
-});
+function changeImage(direction) {
+  currentIndex += direction;
 
-function goLeft() {
-    currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
 
-    if (currentIndex >= myImgs.length) {
-        currentIndex = 0;
-    }
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
 
-    document.getElementById('show_picture').src = "./img/" + myImgs[currentIndex];
-    updateCounter();
-    updateTitle();
+  document.getElementById("show_picture").src =
+    "./img/" + images[currentIndex].img;
+
+  document.getElementById("show_text").textContent = images[currentIndex].title;
+
+  updateCounter();
 }
 
 function goRight() {
-    currentIndex--;
+  changeImage(1);
+}
 
-    if(currentIndex < 0) {
-        currentIndex = myImgs.length - 1;
-    }
-
-    document.getElementById('show_picture').src = "./img/" + myImgs[currentIndex];
-    updateCounter();
-    updateTitle();
+function goLeft() {
+  changeImage(-1);
 }
 
 function updateCounter() {
-    document.getElementById('picture_counter').textContent = currentIndex + 1 + "/12";
+  document.getElementById("picture_counter").textContent =
+    currentIndex + 1 + "/" + images.length;
 }
 
-function updateTitle() {
-    let showText = document.getElementById('show_text');
-    showText.textContent = myTitles[currentIndex];
-}
+renderImages();
