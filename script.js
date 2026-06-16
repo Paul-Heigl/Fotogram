f53f146ff67f16649487644a6a83056a9b2613de
Paul Heigl <paul@heigl.wien>
2026-06-16 13:00:56 +0200

    Finale Version

--- script.js
+++ script.js
@@ -1,86 +1,93 @@
 let dialog = document.getElementById("myDialog");
 let closeBtn = document.getElementById("closeBtn");
 
-let myImgs = [
-  "1.png",
-  "2.png",
-  "3.png",
-  "4.png",
-  "5.png",
-  "6.png",
-  "7.png",
-  "8.png",
-  "9.png",
-  "10.png",
-  "11.png",
-  "12.png",
-];
-
-let myTitles = [
-  "Berglandschaft",
-  "Stadtbild",
-  "Drittes Wolkenhimmel",
-  "Spatz",
-  "Weltall",
-  "Alpen",
-  "Vogel",
-  "Epischer Stein",
-  "Meise",
-  "Leoparden Baby",
-  "Berge",
-  "Baum",
+let images = [
+  { img: "1.png", title: "Berglandschaft" },
+  { img: "2.png", title: "Stadtbild" },
+  { img: "3.png", title: "Drittes Wolkenhimmel" },
+  { img: "4.png", title: "Spatz" },
+  { img: "5.png", title: "Weltall" },
+  { img: "6.png", title: "Alpen" },
+  { img: "7.png", title: "Vogel" },
+  { img: "8.png", title: "Epischer Stein" },
+  { img: "9.png", title: "Meise" },
+  { img: "10.png", title: "Leoparden Baby" },
+  { img: "11.png", title: "Berge" },
+  { img: "12.png", title: "Baum" },
 ];
 
 let currentIndex = 0;
 
-function openDialog(imgName) {
-  currentIndex = myImgs.indexOf(imgName);
-
-  document.getElementById("show_picture").src = "./img/full/" + imgName;
-
-  updateCounter();
-  updateTitle();
-
-  dialog.showModal();
+function init() {
+  renderImages();
+  setupEventListeners();
 }
 
-closeBtn.addEventListener("click", function () {
-  dialog.close();
-});
+function setupEventListeners() {
+  closeBtn.addEventListener("click", function () {
+    dialog.close();
+  });
 
-function goLeft() {
-  currentIndex++;
+  dialog.addEventListener("close", function () { 
+    document.body.classList.remove("dialog-open");
+  });
 
-  if (currentIndex >= myImgs.length) {
+  dialog.addEventListener("click", function (event) {
+    if (event.target === dialog) {
+      dialog.close();
+    }
+  });
+}
+
+function renderImages() {
+  let fotoSection = document.getElementById("foto_section");
+
+  fotoSection.innerHTML = "";
+
+  for (let i = 0; i < images.length; i++) {
+    fotoSection.innerHTML += `
+      <button onclick="openDialog(${i})">
+        <img src="./img/thumbs/${images[i].img}" alt="${images[i].title}" />
+      </button>
+    `;
+  }
+}
+
+function openDialog(index) {
+  currentIndex = index;
+
+  updateDialogImage();
+
+  dialog.showModal();
+  document.body.classList.add("dialog-open");
+}
+
+function changeImage(action) {
+  if (action === "next") {
+    currentIndex++;
+  } else {
+    currentIndex--;
+  }
+
+  if (currentIndex >= images.length) {
     currentIndex = 0;
   }
 
-  document.getElementById("show_picture").src =
-    "./img/full/" + myImgs[currentIndex];
-
-  updateCounter();
-  updateTitle();
-}
-
-function goRight() {
-  currentIndex--;
-
   if (currentIndex < 0) {
-    currentIndex = myImgs.length - 1;
+    currentIndex = images.length - 1;
   }
 
+  updateDialogImage();
+}
+
+function updateDialogImage() {
   document.getElementById("show_picture").src =
-    "./img/full/" + myImgs[currentIndex];
+    "./img/full/" + images[currentIndex].img;
 
-  updateCounter();
-  updateTitle();
-}
+  document.getElementById("show_text").textContent = images[currentIndex].title;
 
-function updateCounter() {
   document.getElementById("picture_counter").textContent =
-    currentIndex + 1 + "/12";
+    currentIndex + 1 + "/" + images.length;
 }
 
-function updateTitle() {
-  document.getElementById("show_text").textContent = myTitles[currentIndex];
-}
+init();
